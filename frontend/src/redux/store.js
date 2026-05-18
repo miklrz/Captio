9f31c521805e39a83c8a3e1c1b59ec2d4c8b0055
hxastur <arz.mikhail@gmail.com>
2026-05-18 15:21:13 +0300

    Add agreement form with redux

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import dialogsReducer from './dialogsSlice';
+import dialogsReducer  from './dialogsSlice';
 import agreementReducer from './agreementSlice';
+import authReducer      from './authSlice';
+import tasksReducer from './tasksSlice';
 
 const store = configureStore({
   reducer: {
-    dialogs: dialogsReducer,
+    dialogs:   dialogsReducer,
     agreement: agreementReducer,
+    auth:      authReducer,
+    tasks: tasksReducer,
   },
 });
 
